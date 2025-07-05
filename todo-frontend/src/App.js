fd30f57735cc53c3de9f87b016baee15ca6743a8
VISHAL PATEL <justmailvishal58@icloud.com>
2025-07-05 18:08:29 +0530

    added redirect to login

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Todolist from "./components/todolist";
+import Home from "./components/home";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/todos" element={<Todolist />} />
       </Routes>
